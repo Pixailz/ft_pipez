ae5b24ecec6f10109b86e12b0e1901c902c8a3a2
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-08-21 01:24:06 +0200

    :non-potable_water: src/pipex_init.c - move pipe(pipex->end) to make sure pipex-end is open if infile   is not present

--- src/pipex_init.c
+++ src/pipex_init.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 19:17:44 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/08/21 00:44:03 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/08/21 01:22:18 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	init_pipex(t_pipex *pipex, char **argv, char **envp)
 	pipex->cmd_success = 0;
 	pipex->cmd1 = get_command(pipex, argv[2]);
 	pipex->cmd2 = get_command(pipex, argv[3]);
+	pipe(pipex->end);
 	pipex->infile = open(argv[1], O_RDONLY);
 	if (pipex->infile < 0)
 	{
@@ -28,5 +29,4 @@ void	init_pipex(t_pipex *pipex, char **argv, char **envp)
 		ft_error(argv[1]);
 	}
 	pipex->outfile = open(argv[4], O_TRUNC | O_CREAT | O_RDWR, 0000644);
-	pipe(pipex->end);
 }
